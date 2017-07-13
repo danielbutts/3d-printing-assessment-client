5527102536be5c80870d54eee22ecdc996d95b6e
danielbutts <daniel.butts@gmail.com>
2017-07-13 00:04:37 -0700

    storing authorization token in window.localStorage

--- public/js/example.env.js
+++ public/js/example.env.js
@@ -2,7 +2,7 @@
   window.__env = window.__env || {};
 
   // API url
-  window.__env.apiUrl = [API_URL];
+  window.__env.apiUrl = 'http://localhost:3000';
 
   // Base url
   window.__env.baseUrl = '/';
--- public/js/user/login.service.js
+++ public/js/user/login.service.js
@@ -6,16 +6,66 @@
 
   service.$inject = ['$http','__env']
   function service($http, __env) {
+    const LOCAL_TOKEN_KEY = 'partridge-auth-token';
+    let isAuthenticated = false;
+    let authToken;
+
     this.authenticate = authenticate
+    this.authenticate = authenticate
+    
 
     function authenticate(login) {
       return $http.post(`${__env.apiUrl}/api/login`, login).then((response) => {
+        storeUserCredentials(response.data.token);
         return response.data;
-        // window.localStorage.setItem(LOCAL_TOKEN_KEY, response.token);
-        // vm.parts = response.data
       })
     }
-
+    
+    function loadUserCredentials() {
+    var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
+    if (token) {
+      useCredentials(token);
+    }
+  }
+ 
+  function storeUserCredentials(token) {
+    window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
+    useCredentials(token);
+  }
+ 
+  function useCredentials(token) {
+    isAuthenticated = true;
+    authToken = token;
+ 
+    // Set the token as header for your requests!
+    $http.defaults.headers.common.Authorization = authToken;
+  }
+ 
+  function destroyUserCredentials() {
+    authToken = undefined;
+    isAuthenticated = false;
+    $http.defaults.headers.common.Authorization = undefined;
+    window.localStorage.removeItem(LOCAL_TOKEN_KEY);
+  }
+ 
+  var register = function(user) {
+    // return $q(function(resolve, reject) {
+    //   $http.post(API_ENDPOINT.url + '/signup', user).then(function(result) {
+    //     if (result.data.success) {
+    //       resolve(result.data.msg);
+    //     } else {
+    //       reject(result.data.msg);
+    //     }
+    //   });
+    // });
+  };
+ 
+  var logout = function() {
+    destroyUserCredentials();
+  };
+ 
+  loadUserCredentials();
+ 
   }
 
 }());
